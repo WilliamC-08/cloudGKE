e8e442242815ea226186306734f958ec47bb5353
WilliamC-08 <71800795+WilliamC-08@users.noreply.github.com>
2021-04-12 22:11:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Hello!\n\nNama Saya: William Chandra\nNIM: 51018021\n, dari jurusan Sistem Informasi :)\n\nTerimakasih!!!')
 })
 
 app.listen(PORT, HOST)
